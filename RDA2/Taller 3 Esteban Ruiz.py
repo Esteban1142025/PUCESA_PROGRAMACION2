5b24890d0ea5bb29f7eb2a2ba73f0d359ecda308
Esteban11042025 <estebanpaul03@gmail.com>
2025-05-30 08:51:45 -0500

    Taller 3

---
+++ RDA2/Taller 3 Esteban Ruiz.py
@@ -0,0 +1,109 @@
+clientes = [
+("Jorja",23.1),
+("Federico",46.2),
+("Rey",53.3),
+("Potts",34.4),
+("Farhan",56.5),
+("Espinoza",26.6),
+("Jean",76.7),
+("Archie",67.8),
+("Donovan",75.9),
+("Mateo",92.1)]
+
+print("Base de datos:")
+for i in clientes:
+    print(i,"\n")
+
+def ordenar_nombres(lista):
+    # Ordenamos los productos según su nombre (posición 0 de la tupla)
+    for i in range(1, len(lista)):
+        actual = lista[i]
+        j = i - 1
+        while j >= 0 and lista[j][0].lower() > actual[0].lower():
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = actual
+    return lista
+
+def ordenar_saldo(lista):
+    n = len(lista)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if lista[j][1] < lista[min_idx][1]:  # Comparamos por precio (índice 1)
+                min_idx = j
+        lista[i], lista[min_idx] = lista[min_idx], lista[i]
+    return lista
+
+def busqueda_nombre(lista_ordenada, objetivo):
+    inicio = 0
+    fin = len(lista_ordenada) - 1
+    comp = 0
+
+    while inicio <= fin:
+        comp += 1
+        medio = (inicio + fin) // 2
+        nombre = lista_ordenada[medio][0].lower()
+
+        if nombre == objetivo.lower():
+            return medio, comp
+        elif objetivo.lower() < nombre:
+            fin = medio - 1
+        else:
+            inicio = medio + 1
+
+    return -1, comp
+
+while True:
+    print("""
+        */\/\/\/\/\/\/\/\ Gestion de Clientes /\/\/\/\/\/\/\/\*
+        Seleccione una de las siguientes opciones:
+        
+        1. Mostrar lista ordenada por nombre
+        2. Mostrar lista ordenada por saldo
+        3. Busqueda de clientes
+        4. Ingresar un nuevo cliente
+        5. Salir del programa
+        """)
+    
+    opcion = input("Ingrese una opcion: ")
+    if opcion == "" or opcion not in ["1", "2", "3", "4", "5"]:
+        print("Error!... No ha ingresado ninguna opcion o la opcion es incorrecta")
+        print("Intentelo nuevamente")
+    
+    if opcion != "5":
+        if opcion == "1":
+            print("Lista de clientes ordenada por nombre:")
+            base_ordenada_N = ordenar_nombres(clientes.copy())
+            for i in base_ordenada_N:
+                print(i)
+                
+        elif opcion == "2":
+            print("Lista de clientes ordenada por saldo:")
+            base_ordenada_S = ordenar_saldo(clientes.copy())
+            for i in base_ordenada_S:
+                print(i)
+                
+        elif opcion == "3":
+            base_ordenada_N = ordenar_nombres(clientes.copy())
+            nombre = input("Ingrese el nombre del cliente a buscar: ")
+            pos, comp = busqueda_nombre(base_ordenada_N, nombre)
+            if pos != -1:
+                print(f"El cliente fue encontrado: {base_ordenada_N[pos]} en la posición {pos}")
+            else:
+                print("Cliente no encontrado.")
+
+            print(f"Se realizaron {comp} comparaciones")
+            
+        elif opcion == "4":
+            print("<<<Agregando un nuevo cliente>>>")
+            cliente = input("Ingrese el nombre del nuevo cliente: ")
+            saldo = float(input("Ingrese el saldo del nuevo cliente: "))
+            clientes.append((cliente,saldo))
+            print("Cliente agregado con exito!")
+            for i in clientes:
+                print(i)
+    else:
+        print("Gracias por usar el programa")
+        print("Adios!")
+        break
